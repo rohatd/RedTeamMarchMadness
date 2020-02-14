ad5958d8b7451c0f6f30f68cae2d9389d8f54d28
Dhruv Rohatgi <rohatd@rpi.edu>
2020-02-14 12:40:18 -0500

    Added Bracket architecture
    bracket architecture backbone

---
+++ bracket.py
@@ -0,0 +1,23 @@
+from queue import Queue
+from matchup import Matchup
+
+
+class Bracket:
+
+    def __init__(self,matchupQueue):
+        if(len(matchupQueue) == 4):
+            self.matchupQueue =matchupQueue
+        else:
+            raise ValueError("Queue did not contain 64 teams")
+
+    def printQueue(self):
+        print(self.matchupQueue)
+
+    def evaluateBracket(self):
+        while len(self.matchupQueue) != 1:
+            team1 = self.matchupQueue.popleft()
+            team2 = self.matchupQueue.popleft()
+            teamMatchup = Matchup(team1,team2)
+            winner = teamMatchup.getWinner()
+            self.matchupQueue.append(winner)
+            print("{0} played {1} and the winner is: {2}".format(team1,team2,winner))
---
+++ matchup.py
@@ -0,0 +1,11 @@
+
+class Matchup:
+
+    def __init__(self,team1,team2):
+        self.team1 = team1 
+        self.team2 = team2 
+
+    
+    def getWinner(self):
+        return self.team1
+        
