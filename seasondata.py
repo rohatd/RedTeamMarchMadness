f576914c4b316b0eec070e58b8dd80a532633a94
Mario F <formim@rpi.edu>
2020-03-31 09:42:19 -1000

    refactored seasondata, extractdata can be deleted

--- seasondata.py
+++ seasondata.py
@@ -1,38 +1,53 @@
 from team import Team
 from matchup import Matchup
 from bracket import *
+import pandas as pd
 
 class SeasonData:
 
-    def __init__(self, year, coach_df=None, bas_df=None, adv_df=None):
-        # Ensure arguments are in order Coach, Basic, Advanced.  All optional for init
+    def __init__(self, year, coach_file, basic_file, adv_file):
         self.teams = {}
         self.year = year
         self.matchups = Queue()
         self.bracket = None  # Initial bracket is blank
 
+        coach_df = None
+        bas_df = None
+        adv_df = None
+        # Load data files
+        try:
+            coach_df = pd.read_csv(coach_file, header=[1])
+        except:
+            print("Error loading coach data csv.")
+        try:
+            bas_df = pd.read_csv(basic_file, header=[1])
+        except:
+            print("Error loading basic data csv.")
+        try:
+            adv_df = pd.read_csv(adv_file, header=[1])
+        except:
+            print("Error loading advanced data csv.")
+
         if (coach_df is not None) and (bas_df is not None):
             # If dataframes are passed, initialize teams.
             # TODO: accommodate adv_df
-            self.initialize_teams(coach_df, bas_df)
 
-    def initialize_teams(self, coach_df, bas_df, adv_df=None):
-        # Iterate through adv and basic data frames to fill
-        # out team informations into Team objects, stored in
-        # self.teams
-        numTeams = bas_df.shape[0]
-        for i in range(numTeams):
-            teamRow = bas_df.loc[i,:]
-            teamName = teamRow['School']
-            # Trim 'NCAA' from the name
-            if "NCAA" in teamName:
-                teamName = teamName[:-5]
-            coachRow = coach_df.loc[coach_df['School'] == teamName]
-            coachName = coachRow['Coach']
-            self.teams[teamName] = Team(self.year, teamRow, coachName)
-        # TODO: accommodate advanced info dataframe by combining it with
-        # teamRow created from basic dataframe.
-        return 0
+            # Iterate through adv and basic data frames to fill
+            # out team informations into Team objects, stored in
+            # self.teams
+            numTeams = bas_df.shape[0]
+            for i in range(numTeams):
+                teamRow = bas_df.loc[i,:]
+                teamName = teamRow['School']
+                # Trim 'NCAA' from the name
+                if "NCAA" in teamName:
+                    teamName = teamName[:-5]
+                coachRow = coach_df.loc[coach_df['School'] == teamName]
+                coachName = coachRow['Coach']
+                self.teams[teamName] = Team(self.year, teamRow, coachName)
+            # TODO: accommodate advanced info dataframe by combining it with
+            # teamRow created from basic dataframe.
+
 
     # Getters
     def get_team(self, teamName):
