5c11d1042ae0c5dd8d74fa99fa87c050733f0d61
Dhruv Rohatgi <rohatd@rpi.edu>
2020-04-16 12:32:59 -0700

    changes bracket.py

--- bracket.py
+++ bracket.py
@@ -1,4 +1,3 @@
-from queue import Queue
 from matchup import Matchup
 import json
 
@@ -6,23 +5,31 @@ import json
 
 class Bracket:
 
-    def __init__(self,matchupQueue):
-        if(len(matchupQueue) == 4):
-            self.matchupQueue =matchupQueue
-        else:
-            raise ValueError("Queue did not contain 64 teams")
-
+    def __init__(self,matchupQ):
+        self.matchupQueue = list(matchupQ)
+        self.matchupList = list(matchupQ)
+        
     def printQueue(self):
         print(self.matchupQueue)
 
+    #returns json of the teams in the queue
     def get_teams(self):
         return json.dummps(list(self.matchupQueue.queue))
 
+    #evalaute bracket 
     def evaluateBracket(self):
         while len(self.matchupQueue) != 1:
-            team1 = self.matchupQueue.popleft()
-            team2 = self.matchupQueue.popleft()
+            team1 = self.matchupQueue.pop(0)
+            team2 = self.matchupQueue.pop(0)
             teamMatchup = Matchup(team1,team2)
-            winner = teamMatchup.getWinner()
+            winner = teamMatchup.randomForestRegressor(2019)
             self.matchupQueue.append(winner)
-            print("{0} played {1} and the winner is: {2}".format(team1,team2,winner))
+            self.matchupList.append(winner)
+            print("{0} played {1} and the winner is: {2}".format(team1.get_team_name().replace(" NCAA","").replace(" ","-"),team2.get_team_name().replace(" NCAA","").replace(" ","-"),winner.get_team_name().replace(" NCAA","").replace(" ","-")))
+
+
+    def getBracket(self):
+        name_list=[]
+        for i in range(len(self.matchupList)):
+            name_list.append(self.matchupList[i].get_team_name().replace(" NCAA",""))
+        return name_list
